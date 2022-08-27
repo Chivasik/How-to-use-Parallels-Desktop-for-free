6f3e765ad1f78964e5c97b46926314659391439d
Arman Nikoyan <71268918+namra004@users.noreply.github.com>
2022-08-27 20:56:35 +0400

    Shell script version to automate all.

---
+++ Parallels_Desktop_Runner.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# Parallels Desktop Runner by https://github.com/namra004
+
+sudo -S date -u 0101043020
+
+cd ~/../../Applications
+
+open ./Parallels\ Desktop.app
+
+sudo -S sntp -sS time.apple.com
+
